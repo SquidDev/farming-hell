c06af6451eb10c2f0670a6c042352ddedb93c2a5
Jonathan Coates <git@squiddev.cc>
2022-02-23 09:00:31 +0000

    Package updates

--- src/data/index.test.ts
+++ src/data/index.test.ts
@@ -2,5 +2,5 @@ import { fsStore } from "../test";
 
 test("Has a basic list of items", async () => {
   const store = await fsStore();
-  expect(store.items.filter(x => x.alwaysDisplay).map(x => x.name)).toMatchSnapshot();
+  expect(store.items.filter(x => x.alwaysDisplay).map(x => `${x.id} => ${x.name}`)).toMatchSnapshot();
 });
